1f66766b049cfc684783255e027798ce04ee2fb3
charles ferté <chferte@hotmail.com>
2013-03-07 18:15:03 -0800

    add mixture modeling framework

--- MEKi_prediction/MEK_framework/y_and yhat_distribution_plots.R
+++ MEKi_prediction/MEK_framework/y_and yhat_distribution_plots.R
@@ -2,16 +2,99 @@
 # sage bionetworks
 # Feb 22th, 2013
 
+
+#############################
+# load the data
+#############################
+source("/home/cferte/FELLOW/cferte/KRAS_Analysis/MEKi_prediction/MEK_framework/load_mek_ccle.R")
+
+
 ################################################################################################################
-# plot the distribution of MEK ActArea (ie: plot the y)
+# compute mixture models to assign sensitive and resistant cells
 ################################################################################################################
 
-par(mfrow=c(2,4),oma=c(0,0,6,0))
+require(flexmix)
+
+Nclust <- 2
+
 cells <- list(mek.cells,nsclc.mek.cells,breast.mek.cells,crc.mek.cells,hemal.mek.cells,glioma.mek.cells,melanoma.mek.cells)
 cell.names <- list("ALL CELLS","NSCLC","BREAST","CRC","Hematologic\nMalignancies","GLIOMA","MELANOMA")
+
+#################
+all.prob <- c()
+all.prob.sens <- c()
+all.prob.res <- c()
+total.cell.status <-c()
 for(i in c(1:length(cells))){
-plot(density(apply(ccle_drug[cells[[i]],mek.inhib],1,mean)),xlim=c(-2,8.5),main=paste(cell.names[[i]]),ylim=c(0,.8),lwd=3)
+  
+  blah <- apply(ccle_drug[cells[[i]],mek.inhib],1,mean)
+  blah <- blah[names(sort(blah))]
+  rank(blah)
+  Nclust <- 2
+  
+  tissue.post.prob <- c()
+  K <- 30
+  while(length(tissue.post.prob)<K) {
+    sample.out <- as.numeric(format(length(names(blah))*.07,digits=1))
+    iter.cells <- sample(names(blah),replace=FALSE,size=length(names(blah))-sample.out)
+    #iter.cells <- sample(rownames(blah),replace=FALSE,size=length(rownames(blah))-2)
+    #tryclust <- flexmix(blah[iter.cells,]~1,k=Nclust, model=FLXMCmvnorm(diag=FALSE))
+    tryclust <- flexmix(blah[iter.cells]~1,k=Nclust)
+    clust <- tryclust@cluster
+    table(clust)
+    if(length(table(clust))<Nclust){} else{
+    names(clust) <- iter.cells
+    
+    post.prob <- tryclust@posterior$scaled
+    if(mean(blah[names(which(clust==1))]) > mean(blah[names(which(clust==2))])) {
+      colnames(post.prob) <- c("sens","res")
+    } else { colnames(post.prob) <- c("res","sens")}
+    post.prob <- post.prob[,c("sens","res")]
+    rownames(post.prob) <- iter.cells
+    tissue.post.prob  <- c(tissue.post.prob,list(post.prob))
+    }
+  }
+  
+
+final.prob.sens <- matrix(NA,ncol=length(tissue.post.prob),nrow=length(cells[[i]])) 
+rownames(final.prob.sens) <- names(blah)
+for(x in  1:length(tissue.post.prob)){final.prob.sens[rownames(tissue.post.prob[[x]]),x]<-(tissue.post.prob[[x]][,"sens"])}
+  
+  all.prob.sens <- c(all.prob.sens,list(final.prob.sens))
+  
+  final.prob.res <- matrix(NA,ncol=length(tissue.post.prob),nrow=length(cells[[i]])) 
+  rownames(final.prob.res) <- names(blah)
+  for(x in  1:length(tissue.post.prob)){final.prob.res[rownames(tissue.post.prob[[x]]),x]<-(tissue.post.prob[[x]][,"res"])}
+  all.prob.res <- c(all.prob.res,list(final.prob.res))
+  
+final.prob <- cbind(apply(final.prob.sens,1,function(x){exp(mean(log(x[!is.na(x)])))}),apply(final.prob.res,1,function(x){exp(mean(log(x[!is.na(x)])))}))
+  colnames(final.prob) <- c("sens","res")
+  
+  all.prob <- c(all.prob,list(final.prob))
+
+  }
+
+
+###################
+# plot it 
+##################
+par(mfrow=c(2,4),oma=c(0,0,0,0))
+for(i in 1:length(all.prob))
+{
+#col.vec <- (apply(all.prob[[i]],1,function(x){names(which(x==max(x)))}))
+#col.vec <- ifelse(col.vec=="sens",1,2)
+col.vec <- factor(-1*(all.prob[[i]][,1]+1))
+plot(density(apply(ccle_drug[cells[[i]],mek.inhib],1,mean)),lwd=3,main=paste(cell.names[[i]]),xlim=c(-1.5,8),ylim=c(0,1))
+ypos <- abs(rnorm(sd=.04,mean=.8,n=length(rownames(all.prob[[i]]))))
+points(apply(ccle_drug[rownames(all.prob[[i]]),mek.inhib],1,mean),ypos,col=greenred(length(col.vec))[col.vec],pch=20,cex=1)
 }
+
+
+# cell status: sensitive are the ones >.6, res are <.4, inter are the rest !
+cell.status <- sapply(1:length(cell.names),function(x){ifelse(all.prob[[x]][,1]>.6,"sens",ifelse(all.prob[[x]][,1]<.4,"res","inter"))})
+
+
+###########################################################################################################################
 tmp <- sapply(c(1:N),function(x){return(PARAL[[x]][[2]])})
 plot(density(apply(ccle_drug[tmp,mek.inhib],1,mean)),xlim=c(-2,8.5),main="training set",ylim=c(0,.8),lwd=3,col="red")
 title(main="Distribution of the sensitivity of cell lines to MEK inhibitors according to tissue type \n(sensitivity assessed by ActArea)",
